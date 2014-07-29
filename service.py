548f30292b520a469d53133d9b94b39311cf0855
Dmitry Ogurtsov <zorbis11@gmail.com>
2014-07-29 19:06:31 +0300

    reqs added

--- service.py
+++ service.py
@@ -12,13 +12,6 @@ define("port", default=8888, help="run on the given port", type=int)
 define("debug", default=True, help="run in debug mode")
 
 
-class RequestBodyParser():
-    data = {}
-    def __init__(self, request):
-        self.data = json.loads(request.body)
-    def get(self, field, default=None):
-        return self.data.get(field, default)
-
 class DemoHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
@@ -30,11 +23,6 @@ class PubSub(sockjs.tornado.SockJSConnection):
     channels = {}
 
     def on_open(self, request):
-        # Send that someone joined
-        #self.broadcast(self.participants, "Someone joined.")        
-
-        # Add client to the clients list
-        #self.participants.add(self)
         pass
 
     def on_message(self, message):
@@ -43,7 +31,6 @@ class PubSub(sockjs.tornado.SockJSConnection):
         except:
             data = {}
 
-        # method router (todo: switch to switch)
         method = data.get('method', 'no-method')
         if method == 'broadcast':
             self._broadcast(data.get('data', {}))
@@ -64,11 +51,9 @@ class PubSub(sockjs.tornado.SockJSConnection):
             self.channels[self.channel] = set()            
         self.channels[self.channel].add(self)
 
-    def on_close(self):
-        # Remove client from the clients list and broadcast leave message
-        self.participants.remove(self)
+    def on_close(self):        
+        self.channels[self.channel].remove(self)
 
-        self.broadcast(self.participants, "Someone left.")
 
 def main():
     parse_command_line()
@@ -83,6 +68,7 @@ def main():
         )
     app.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
     main()
